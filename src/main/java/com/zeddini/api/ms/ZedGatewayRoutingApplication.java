3a2a26008b79b8366ea19ebae60dff5abca4c327
Walid ZEDDINI <w.zeddini@gmail.com>
2023-06-29 23:34:23 +0100

    First Commit

---
+++ src/main/java/com/zeddini/api/ms/ZedGatewayRoutingApplication.java
@@ -0,0 +1,39 @@
+package com.zeddini.api.ms;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ZedGatewayRoutingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZedGatewayRoutingApplication.class, args);            // it wil start application
+    }
+}
+
+@Configuration
+ class CorsConfiguration{
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        org.springframework.web.cors.CorsConfiguration corsConfig = new org.springframework.web.cors.CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+ }   
